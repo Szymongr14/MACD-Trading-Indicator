5185bd8c815d87e2c542c742bd3a95a0fd7b3957
Szymon Groszkowski <szymongr14@gmail.com>
2024-03-10 11:14:18 +0100

    add simulating macd indicator

--- convert_data.py
+++ convert_data.py
@@ -3,9 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def get_data(file_name, title):
+def get_data(file_name, title, day_first):
     df = pd.read_csv(file_name)
-    dates = pd.to_datetime(df['Date'])
+    if day_first:
+        dates = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+    else:
+        dates = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+
     prices = df['Closing']
     plt.title('Historical Rates : ' + title)
     plt.xlabel('Date')
@@ -13,3 +17,72 @@ def get_data(file_name, title):
     plt.plot(dates, prices, color="purple")
     plt.show()
     return df
+
+
+def count_macd(df, title, day_first):
+    df['EMA12'] = df['Closing'].ewm(span=12, adjust=False).mean()
+    df['EMA26'] = df['Closing'].ewm(span=26, adjust=False).mean()
+    df['MACD'] = df['EMA12'] - df['EMA26']
+    df['Signal_Line'] = df['MACD'].ewm(span=9, adjust=False).mean()
+
+    for i in range(0, len(df) - 1):
+        current_row = df.iloc[i]
+        next_row = df.iloc[i + 1]
+        if current_row['MACD'] < current_row['Signal_Line'] and next_row['MACD'] > next_row['Signal_Line']:
+            df.at[i, 'buy'] = df.at[i, 'MACD']
+            df.at[i, 'sell'] = 0
+        elif current_row['MACD'] > current_row['Signal_Line'] and next_row['MACD'] < next_row['Signal_Line']:
+            df.at[i, 'sell'] = df.at[i, 'MACD']
+            df.at[i, 'buy'] = 0
+        else:
+            df.at[i, 'buy'] = 0
+            df.at[i, 'sell'] = 0
+
+    buy_data = df[df['buy'] != 0]
+    sell_data = df[df['sell'] != 0]
+    buy_points = buy_data['buy']
+    sell_points = sell_data['sell']
+    if day_first:
+        dates_for_sell_points = pd.to_datetime(sell_data['Date'], format='%d/%m/%Y')
+        dates_for_buy_points = pd.to_datetime(buy_data['Date'], format='%d/%m/%Y')
+        dates = pd.to_datetime(df['Date'], format='%d/%m/%Y')
+    else:
+        dates_for_sell_points = pd.to_datetime(sell_data['Date'], format='%Y-%m-%d')
+        dates_for_buy_points = pd.to_datetime(buy_data['Date'], format='%Y-%m-%d')
+        dates = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+
+    prices_averages = df['MACD']
+    plt.title('MACD for : ' + title)
+    plt.xlabel('Date')
+    plt.ylabel('Price Averages')
+    plt.plot(dates, df['Signal_Line'], color="red", label='signal line')
+    plt.plot(dates, prices_averages, label='MACD')
+    plt.scatter(dates_for_buy_points, buy_points, color='green', marker='x', label='buy')
+    plt.scatter(dates_for_sell_points, sell_points, color='red', marker='x', label='sell')
+    plt.legend()
+    plt.show()
+
+
+def simulate_macd_strategy(df, start_units):
+    actions = start_units
+    sold_funds = False
+
+    for i in range(1, len(df)):
+        previous_price = df.iloc[i - 1]
+        current_price = df.iloc[i]
+        if df.at[i, 'buy'] == 1.0 and sold_funds:
+            sold_funds = False
+            continue
+
+        elif df.at[i, 'sell'] == 1.0 and not sold_funds:
+            sold_funds = True
+            continue
+
+        if not sold_funds:
+            actions = actions * (current_price['Closing'] / previous_price['Closing'])
+
+    return actions
+
+
+def simulate_alternative_strategy(df, start_units):
+    print('a')
--- main.py
+++ main.py
@@ -1,30 +1,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from convert_data import get_data
+from convert_data import get_data, count_macd, simulate_macd_strategy, simulate_alternative_strategy
 
 
-def count_macd(df, title):
-    # Calculate the 12-period EMA
-    df['EMA12'] = df['Closing'].ewm(span=12, adjust=False).mean()
-    # Calculate the 26-period EMA
-    df['EMA26'] = df['Closing'].ewm(span=26, adjust=False).mean()
-    # Calculate MACD (the difference between 12-period EMA and 26-period EMA)
-    df['MACD'] = df['EMA12'] - df['EMA26']
-    # Calculate the 9-period EMA of MACD (Signal Line)
-    df['Signal_Line'] = df['MACD'].ewm(span=9, adjust=False).mean()
+UAXUSD_data_frame = get_data('xauusd.csv', 'Gold (ozt) / U.S. Dollar', True)
+WIG20_data_frame = get_data('wig20.csv', 'WIG20', False)
+count_macd(UAXUSD_data_frame, 'UAX_to_USD', True)
+count_macd(WIG20_data_frame, 'WIG20', False)
 
-    dates = pd.to_datetime(df['Date'])
-    prices_averages = df['MACD']
-    plt.title('MACD for : ' + title)
-    plt.xlabel('Date')
-    plt.ylabel('Price Averages')
-    plt.plot(dates, df['Signal_Line'], color="red")
-    plt.plot(dates, prices_averages)
-    plt.show()
-
-
-UAX_to_USD_graph = get_data('xauusd.csv', 'Gold (ozt) / U.S. Dollar')
-WIG20_graph = get_data('wig20.csv', 'WIG20')
-count_macd(UAX_to_USD_graph, 'UAX_to_USD')
-count_macd(WIG20_graph, 'WIG20')
+start_units = 1000
+final_capital_WIG20 = simulate_macd_strategy(WIG20_data_frame, start_units)
+final_capital_UAXUSD = simulate_macd_strategy(UAXUSD_data_frame, start_units)
+print("WIG20 Actions after using MACD: {}%".format(round(final_capital_WIG20 / start_units * 100), 2))
+print("UAXUSD Actions after using MACD: {}%".format(round(final_capital_UAXUSD / start_units * 100), 2))
