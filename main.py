0caa9b7b418eccd75878b6ee6513722c6b84d24e
Szymon Groszkowski <szymongr14@gmail.com>
2024-03-11 21:27:20 +0100

    clean main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from convert_data import get_data, count_macd, simulate_macd_strategy, simulate_alternative_strategy
 
 
@@ -12,5 +9,9 @@ count_macd(WIG20_data_frame, 'WIG20', False)
 start_units = 1000
 final_capital_WIG20 = simulate_macd_strategy(WIG20_data_frame, start_units)
 final_capital_UAXUSD = simulate_macd_strategy(UAXUSD_data_frame, start_units)
+final_alternative_capital_WIG20 = simulate_alternative_strategy(WIG20_data_frame, start_units)
+final_alternative_capital_UAXUSD = simulate_alternative_strategy(UAXUSD_data_frame, start_units)
 print("WIG20 Actions after using MACD: {}%".format(round(final_capital_WIG20 / start_units * 100), 2))
 print("UAXUSD Actions after using MACD: {}%".format(round(final_capital_UAXUSD / start_units * 100), 2))
+print("WIG20 Actions after using MACD based strategy: {}%".format(round(final_capital_WIG20 / start_units * 100), 2))
+print("UAXUSD Actions after using MACD based strategy: {}%".format(round(final_capital_UAXUSD / start_units * 100), 2))
